0a2722b10e955a72ae3a46969daf58199b205132
Amirkhaksar <amirkhaksar81@outlook.com>
2022-05-06 14:46:15 +0430

    Update AColoBox

--- A color box/Acolorbox.py
+++ A color box/Acolorbox.py
@@ -1,3 +1,11 @@
+"""
+#Language:Python
+#ID:AColoBox
+#QLink:https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/paint-box-3e9796fd/
+#All Accepet
+#Author:Amirkhaksar
+"""
+
 lk = list(map(int, input().split()))
 n, k = lk[0], lk[1]
 a = list(map(int, input().split()))
