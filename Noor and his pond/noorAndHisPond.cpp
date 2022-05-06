62dcd1f80ca58026c62b4731b357839c87c1f680
Amir Mohammad Aghazadeh <cssu.ama@gmail.com>
2022-05-06 12:55:11 +0430

    adding question number 113, 119, & 125 to repo

---
+++ Noor and his pond/noorAndHisPond.cpp
@@ -0,0 +1,56 @@
+/*
+	Author: Amir Aghazadeh
+	Lan: cpp
+	Id: 125
+	Qlink: https://www.hackerearth.com/practice/algorithms/sorting/quick-sort/practice-problems/algorithm/noor-and-his-pond-760eabe0/
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+	int T;
+	cin >> T;
+
+	int counter = 0;
+	
+	for(int i = 0; i < T; i++)
+	{
+		int N;
+		cin >> N;
+		
+		vector<pair<int , int>> vector;
+		for(int j = 0; j < N; j++)
+		{
+			int 
+				S,
+				E;
+				
+			cin
+				>> S
+				>> E;
+				
+			vector.push_back({E, 1});	
+			vector.push_back({S, -1});
+		}
+		
+		sort(vector.begin(), vector.end());
+		
+		int
+			answer = 0,
+			sum = 0;
+			
+		for(int j = 0; j < vector.size(); j++)
+		{
+			sum += vector[j].second;
+			answer = max(answer, sum);
+		}
+		
+		cout << answer << endl;
+	}	
+}
