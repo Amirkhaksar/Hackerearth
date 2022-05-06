1ebb8d1f588d189cc94ef6bb1fe0b36887d7985c
Amirkhaksar <amirkhaksar81@outlook.com>
2022-05-06 14:58:29 +0430

    Update Maximum Sum of Building Speed

--- Maximum Sum of Building Speed/MaximumSumofBuildingSpeed.py
+++ Maximum Sum of Building Speed/MaximumSumofBuildingSpeed.py
@@ -1,3 +1,10 @@
+"""
+#Language:Python
+#ID:Maximum Sum of Building Speed
+#QLink:https:https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/practice-problems/algorithm/maximum-sum-of-building-speed-00ab8996/
+#All Accepet
+#Author:Amirkhaksar
+"""
 n=int(input())
 
 a=list(map(int,input().split()))
