52033dde88d364e3b787a447ca51c9a3c1bf45a5
Amir Mohammad Aghazadeh <cssu.ama@gmail.com>
2022-05-20 12:23:28 +0430

    [UPDATING] edit my solution and add new comment for question number 167

--- Travel/travel.cpp
+++ Travel/travel.cpp
@@ -3,6 +3,7 @@
 	QLink: https://www.hackerearth.com/practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/practice-problems/algorithm/travel-2-faeafba1/
 	id: 167
 	author: amir aghazadeh
+	partialy accepted
 */
 
 
