15318ec5c9d574fcafa35883a2750e79a597e7ce
unknown <mahdiabbasi602@gmail.com>
2022-06-09 23:34:22 +0430

    Build A Graph

---
+++ buildAGraph/buildAGraph.cpp
@@ -0,0 +1,21 @@
+//Language: cpp
+//ID: 212
+//Qlink: https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/algorithm/build-a-graph-5f5c6b4a/
+//Author: @mahdimma
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	if (N > 11)
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		cout << "No";
+	}
+	return 0;
+}
