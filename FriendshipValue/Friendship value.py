de7ba35e67251f1e241b7c6f6dcd8cec730a7d23
amir <hejaziamirmohammad5@gmail.com>
2022-06-17 17:58:00 +0430

    'Friendship-value'

---
+++ FriendshipValue/Friendship value.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+language = python
+
+id = Friendship value
+
+qlink = https://www.hackerearth.com/practice/algorithms/graphs/graph-representation/practice-problems/approximate/freindship-and-covid-2-b2d978ae/
+
+author: amir hejazi
+'''
+k, n, m = map(int, input().split())
+print(' '.join(str(i) for i in range(1, k + 1)))
