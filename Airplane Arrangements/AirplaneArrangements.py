77c1eac109464666a8aacd55fc3119698ad16ea5
Amirkhaksar <amirkhaksar81@outlook.com>
2022-07-21 21:36:58 +0430

    [ADD] Airplane Arrangements (296)

---
+++ Airplane Arrangements/AirplaneArrangements.py
@@ -0,0 +1,13 @@
+
+#Language:C++
+#ID:Airplane Arrangements
+#QLink:https://www.hackerearth.com/problem/algorithm/the-furious-five-69521576/
+#All Accepet
+#Author:Amirkhaksar
+
+
+import math
+mod = 1000000007
+n, m = map(int, input().split())
+
+print (pow(2*(n+1), m, mod) * pow(n+1, mod-2, mod) % mod * (n+1-m) % mod)
